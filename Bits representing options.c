096741c1d20aeb706308086e83977804d1ac601a
johndoe83104 <johndoe83104@gmail.com>
2021-03-31 18:22:18 +0300

    Update Bits representing options.c

--- Bits representing options.c
+++ Bits representing options.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 
+// Option definitions
 #define ERROR_MSG   0x01   // 0b00000001
 #define WARNING_MSG 0x02   // 0b00000010
 #define INFO_MSG    0x04   // 0b00000100
@@ -10,6 +11,7 @@ void showMessage(uint8_t options);
 
 int main(){
 
+  // The functions will display one or more of the options that are passed as parameters
   showMessage( ERROR_MSG | WARNING_MSG | INFO_MSG );
   
   return 0;
